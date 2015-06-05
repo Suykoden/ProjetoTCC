c2845ebd8cb66083888cec46b7998a7b8e488774
Suykoden <damasio321@yahoo.com.br>
2015-06-05 14:22:33 -0300

    classe account

---
+++ LetsParty.UI.Web/Models/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace LetsParty.UI.Web.Models
+{
+    public class Account
+    {
+
+        
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Usuario")]
+        public string Usuario { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+
+        public string Senha { get; set; }
+    }
+}
