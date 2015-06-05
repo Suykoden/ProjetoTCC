dc50e411083be875b6ff76954ae5d018477fd754
Rafael Damasio <damasio321@yahoo.com.br>
2015-06-05 20:08:01 -0300

    implementação da Entity e db migrations

--- slnTCC.Domain/Entities/Cliente.cs
+++ slnTCC.Domain/Entities/Cliente.cs
@@ -1,4 +1,4 @@
-﻿using System
+﻿using System;
 namespace slnTCC.Domain.Entities
 {
     public class Cliente
---
+++ slnTCC.Infra.Data/Contexto/ProjectContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using slnTCC.Domain.Entities;
+namespace slnTCC.Infra.Data.Contexto
+{
+    public class ProjectContext : DbContext
+        
+    {
+        public ProjectContext()
+            : base("BancoTCC")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+}
