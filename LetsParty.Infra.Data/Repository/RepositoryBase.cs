5880295a10d0fab2f69dfaa726d95c330342970f
Suykoden <damasio321@yahoo.com.br>
2015-06-05 10:48:17 -0300

    teste

---
+++ LetsParty.Infra.Data/Repository/RepositoryBase.cs
@@ -0,0 +1,65 @@
+﻿using LetsParty.Infra.Data.Context;
+using LetsParty.Seedwork;
+using LetsParty.Seedwork.Repository;
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace LetsParty.Infra.Data.Repository
+{
+
+
+    public abstract class RepositoryBase<T> : IRepository<T> where T : EntityBase
+    {
+
+        private DbContext Context { get; set; }
+        private DbSet<T> Base { get; set; }
+
+        public RepositoryBase(ILetsPartyContext currentContext) {
+            Context = currentContext.Context;
+            Base = currentContext.Context.Set<T>();
+        }
+
+        public IQueryable<T> All()
+        {
+            return Base.AsQueryable();
+        }
+
+        public T GetById(Guid? id)
+        {
+            if (!id.HasValue) return null;
+
+            return All().AsQueryable().FirstOrDefault(p => p.Id == id.Value);
+        }
+
+        public void Insert(T entity)
+        {
+            Base.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            Context.Entry<T>(entity).State = System.Data.Entity.EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            if (entity == null) throw new NullReferenceException("Entidade que deveria ser excluida não foi passada para o metodo do repositorio");
+
+            Base.Remove(entity);
+        }
+
+        public void Delete(Guid entityId)
+        {
+            var entity = GetById(entityId);
+            if (entity == null) throw new NullReferenceException("Não foi possivel encontrar o registro com chave informada.");
+
+            Delete(entity);            
+        }
+    }
+
+
+}
+
+
+ 
---
+++ LetsParty.Seedwork/DTO/DtoWithCode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsParty.Seedwork.DTO
+{
+    public class DtoWithCode : Dto
+    {
+        public string Codigo { get; set; }
+    }
+}
