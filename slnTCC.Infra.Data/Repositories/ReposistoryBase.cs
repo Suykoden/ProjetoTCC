082225648e1cd2c1c9a4f0be710b0c7b96e4f2fb
Suykoden <damasio321@yahoo.com.br>
2015-06-08 13:04:23 -0300

    .

--- slnTCC.Infra.Data/Repositories/ReposistoryBase.cs
+++ slnTCC.Infra.Data/Repositories/ReposistoryBase.cs
@@ -4,19 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using slnTCC.Domain.Interfaces;
+using slnTCC.Infra.Data.Contexto;
+
 namespace slnTCC.Infra.Data.Repositories
 {
-    public class ReposistoryBase<TEntity>:IDisposable,IRepositorioBase<TEntity> where TEntity :class
+    public class ReposistoryBase<TEntity>:IDisposable,IRepositorioBase<TEntity> where TEntity :class ////classe concreta que implementa a interface repositorio base e idisposable para que a mesma possa ser destruida
     {
+
+        protected ProjectContext Db = new ProjectContext(); ///Instancia da minha classe implementada no meu contexto 
+
+
 
         public void add(TEntity obj)
         {
-            throw new NotImplementedException();
+            Db.Set<TEntity>().Add(obj);   ///Salva um tipo generico que será definido na hora inserção 
+            Db.SaveChanges();
         }
 
         public TEntity GetById(int id)
         {
-            throw new NotImplementedException();
+            return Db.Set<TEntity>().Find(id);
         }
 
         public IEnumerable<TEntity> GetAll()
