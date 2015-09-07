a2ce579c69cbd41e3c3fc45bfc135aa97d9e39e2
Rafael Damasio <damasio321@yahoo.com.br>
2015-09-07 14:37:54 -0300

    View anúncio

--- LetsParty.UI.Web/Controllers/HomeController.cs
+++ LetsParty.UI.Web/Controllers/HomeController.cs
@@ -42,7 +42,10 @@ namespace LetsParty.UI.Web.Controllers
         {
             return View();
         }
-        
+        public ActionResult Anuncio()
+        {
+            return View();
+        }
 
     }
 }
