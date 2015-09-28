f9b0415a0828e8164281d35176da9500ec067851
Suykoden <damasio321@hotmail.com>
2015-09-28 18:01:56 -0300

    Acerto BD

--- LetsParty.Domain/Model/Atores/Usuario.cs
+++ LetsParty.Domain/Model/Atores/Usuario.cs
@@ -9,39 +9,32 @@ namespace LetsParty.Domain.Model.Atores
 {
     public class Usuario : EntityWithCodeAndName
     {
-        public string senha { get; set; }
-        public string email { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
         public DateTime DataCadastro { get; set; }
+        public string Documento { get; set; }
+        public string Telefone { get; set; }
+        public string Celular { get; set; }
+        public string Ramo { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Endereco { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Cep { get; set; }
+        public string Pais { get; set; }
+        public string Ponto_Referencia { get; set; }
+        public string Imagem { get; set; }
+        public string Ativo { get; set; }
+
+
+       
 
 
 
-
-        public void EfetuarLogin()
-        {
-
-            //Implementar login aqui 
-        }
-
-        public void GerenciarConta()
-        {
-
-            //Implementar gerenciamento de contas 
-        }
-
-        public void GeraRelatorio()
-        {
-
-            ///Implementar geração de relatórios ,somente para fornecedor e administrador do sistema 
-
-
-
-        }
-        public void GerenciarMensagem()
-        {
-
-            //Implementar gerenciamento de mensagens 
-        }
-
+       
 
     }
 }
